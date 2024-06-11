6c298a4feeee0156dbed2997ecef00861f772498
lhbizzi <106413189+lhbizzi@users.noreply.github.com>
2024-06-10 21:36:00 -0300

    Criação-get-card

--- src/Components/Cards/index.js
+++ src/Components/Cards/index.js
@@ -1,14 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './cards.css'; // Certifique-se de que o caminho está correto
+import { api } from '../../utils/api';
 
 function Cards() {
-    return (
-        <div className="card">
-            <div className="card-details">
-                <p className="text-title">Card title</p>
-                <p className="text-body">Here are the details of the card</p>
+    const [dados, setDados] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        api
+            .get("task")
+            .then((response) => {
+                console.log("Resposta da API:", response.data);
+                // Certifique-se de que response.data.task é um array
+                if (Array.isArray(response.data.task)) {
+                    setDados(response.data.task);
+                } else {
+                    console.error("A resposta da API não contém um array esperado:", response.data);
+                    setDados([]);
+                }
+            })
+            .catch((error) => {
+                console.error("Erro ao buscar dados da API:", error);
+                setError("Erro ao buscar dados");
+                setDados([]);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) {
+        return <p>Carregando...</p>;
+    }
+
+    if (error) {
+        return <p>{error}</p>;
+    }
+    console.log(dados);
+    if (dados.length === 0) {
+        return (
+            <div className="no-tasks">
+                <p>--Sem tarefas criadas--</p>
             </div>
-            <button className="card-button">More info</button>
+        );
+    }
+    return (
+        <div>
+            {dados.map((dados) => (
+                <div key={dados.id} className="card">
+                    <div className="card-details">
+                        <p className="text-title text-opacity">{dados.title}</p>
+                        <p className="text-body text-opacity">Data limite de finalização: {dados.data_tarefa}</p>
+                        <p className="text-body text-opacity">Status: {dados.fg_ativo ? 'Ativo' : 'Inativo'}</p>
+                    </div>
+                    <button className="card-button">More info</button>
+                </div>
+            ))}
         </div>
     );
 }
